e3104f2ff072df17158e055ce15aa8308ce50fee
Benjamin Z <benzhai21@gmail.com>
2022-02-11 11:34:29 -0700

    kfp

--- script.js
+++ script.js
@@ -14,29 +14,31 @@ function characterSearch(){
 
     // Get Input
     let name = document.getElementById("name").value;
+    name = name.toLowerCase();
+
 
     // Check name
-    if (name === "Tigress") {
+    if (name === "tigress") {
         charImgEl.src = "images/tigress.png";
         charNameEl.innerHTML = "Tigress"
         charQuoteEl.innerHTML = "That was pretty hardcore!"
-    } else if (name === "Viper"){
+    } else if (name === "viper"){
         charImgEl.src = "images/viper.png";
         charNameEl.innerHTML = "Viper"
         charQuoteEl.innerHTML = "I don't need to bite to fight!"
-    }else if (name === "Po"){
+    }else if (name === "po"){
         charImgEl.src = "images/po.png";
         charNameEl.innerHTML = "Po"
         charQuoteEl.innerHTML = "Buddy, I am the Dragon Warrior!"
-    }else if (name === "Mantis"){
+    }else if (name === "mantis"){
         charImgEl.src = "images/mantis.png";
         charNameEl.innerHTML = "Mantis"
         charQuoteEl.innerHTML = "Fear the bug!"
-    }else if (name === "Monkey"){
+    }else if (name === "monkey"){
         charImgEl.src = "images/monkey.png";
         charNameEl.innerHTML = "Monkey"
         charQuoteEl.innerHTML = "We should hang out!"
-    }else if (name === "Crane"){
+    }else if (name === "crane"){
         charImgEl.src = "images/crane.png";
         charNameEl.innerHTML = "Crane"
         charQuoteEl.innerHTML = "You can chain my body, but you will never chain my warrior spirit!"
